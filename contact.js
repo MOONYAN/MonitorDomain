08f76451ffb05051459e02e32040c878801a064a
MOONYAN <1787hz@gmail.com>
2018-03-31 17:22:58 +0800

    contact class

--- contact.js
+++ contact.js
@@ -3,11 +3,6 @@ module.exports = class {
         this.id = contact.id;
         this.name = contact.name;
         this.email = contact.email;
-        this.address = contact.address || {};
-        this.hosts = contact.hosts || [];
-    }
-
-    monitor(host) {
-        this.hosts.push(host);
+        this.address = contact.address;
     }
 };
