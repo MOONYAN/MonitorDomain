e5b20609c8b0961960efe8a33bba7e97e79cbda8
MOONYAN <1787hz@gmail.com>
2018-03-31 17:26:00 +0800

    contact service

---
+++ contactService.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var connection = mongoose.connect('mongodb://Sasuke:9487@ds033126.mlab.com:33126/narutomongo');
+
+var ContactSchema = require('./Schemas/contactSchema');
+var Contact = require('./contact');
+
+var self = {};
+
+self.addContact = async (contact) => {
+    try {
+        return new Contact(await ContactSchema.create({
+            name: contact.name,
+            email:contact.email,
+            address:contact.address
+        }));
+    } catch (err) {
+        throw err;
+    }
+};
+
+self.getContacts = async () => {
+    try {
+        let contacts = await ContactSchema.find().exec();
+        return contacts.map(e => new Contact(e));
+    } catch (err) {
+        throw err;
+    }
+};
+
+module.exports = self;
