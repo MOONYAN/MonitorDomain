0b8cba00ca9df1d4325a6088b03f567c4538ea0e
MOONYAN <1787hz@gmail.com>
2018-04-02 17:44:25 +0800

    add use case find host by id

--- routes/hosts.js
+++ routes/hosts.js
@@ -3,12 +3,20 @@ var router = express.Router();
 var domain = require('../domain/domain');
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', async function (req, res) {
     try {
         res.json(await domain.getHosts());
     } catch (err) {
         res.json(err);
-    }    
+    }
 });
+
+router.get('/:id', async function (req, res) {
+    try {
+        res.json(await domain.findHost(req.params.id));
+    } catch (err) {
+        res.json(err);
+    }
+})
 
 module.exports = router;
