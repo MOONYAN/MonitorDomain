d72fe7db17b5a9f7c87605848b5121ba8e900411
MOONYAN <1787hz@gmail.com>
2018-04-02 23:34:54 +0800

    add use case update host command

--- routes/hosts.js
+++ routes/hosts.js
@@ -5,17 +5,27 @@ var domain = require('../domain/domain');
 /* GET users listing. */
 router.get('/', async function (req, res) {
     try {
-        res.json(await domain.getHosts());
+        return res.json(await domain.getHosts());
     } catch (err) {
-        res.json(err);
+        return res.json(err);
     }
 });
 
 router.get('/:id', async function (req, res) {
     try {
-        res.json(await domain.findHost(req.params.id));
+        return res.json(await domain.findHost(req.params.id));
     } catch (err) {
-        res.json(err);
+        return res.json(err);
+    }
+})
+
+router.put('/:id', async function (req, res) {
+    try {
+         let host = await domain.findHost(req.params.id);
+         host.command = req.body.command;        
+        return res.json(await domain.updateHostCommand(host));
+    } catch (err) {
+        return res.json(err);
     }
 })
 
