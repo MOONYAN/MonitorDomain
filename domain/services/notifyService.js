db76dae007c28c97ceb3d1b8720d065230c30244
MOONYAN <1787hz@gmail.com>
2018-05-12 22:35:54 +0800

    add notifyService test case

--- domain/services/notifyService.js
+++ domain/services/notifyService.js
@@ -6,8 +6,7 @@ module.exports = {
         senders[key] = sender;
     },
 
-    notify(receiver, message) {
-        let address = receiver.address;
+    notify({address}, message) {
         for (let key in address) {
             if (senders[key]) {
                 senders[key].send(address[key], message);
