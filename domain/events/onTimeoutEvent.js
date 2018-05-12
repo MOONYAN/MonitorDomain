5c969052d863784afdaae6a1d65593be3328f231
MOONYAN <1787hz@gmail.com>
2018-05-12 14:07:07 +0800

    revise time stamp

--- domain/events/onTimeoutEvent.js
+++ domain/events/onTimeoutEvent.js
@@ -1,8 +1,6 @@
-var timestamp = require('time-stamp');
-
 module.exports = class {
     constructor() {
         this.eventType = 'onTimeoutEvent';
-        this.argument = timestamp('YYYY/MM/DD HH:mm:ss');
+        this.argument = new Date().toTimeString();
     }
 };
