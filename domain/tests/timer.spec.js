5db3f447d5d4e6f7ffe0f640feadc1ce6edc1a36
MOONYAN <1787hz@gmail.com>
2018-05-12 19:20:17 +0800

    timer test case

---
+++ domain/tests/timer.spec.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const should = chai.should();
+const Timer = require('../timer');
+const EventEmitter = require('events');
+
+describe('Timer', _ => {
+    it('should get timeout events', done => {
+        let emitter = new EventEmitter();
+        emitter.once('timeout', _ => {
+            timer.stop();
+            done();
+        });
+        let timer = new Timer(emitter);
+        timer.start(1);
+    });
+});
