bbac1ed863cee9bc4ab3da0b6ecfa80db5c92e65
MOONYAN <1787hz@gmail.com>
2018-05-13 13:44:30 +0800

    revise with respect to monitorService DI

--- domain/domain.js
+++ domain/domain.js
@@ -9,14 +9,18 @@ const notifyService = require('./services/notifyService');
 notifyService.use('Email', require('./services/mailService'));
 notifyService.use('Line', require('./services/lineService'));
 notifyService.use('FB', require('./services/fbService'));
-notifyService.use('Phone',require('./services/phoneService'));
+notifyService.use('Phone', require('./services/phoneService'));
 
 let eventEmitter = new EventEmitter();
-let hostEventHandler = new HostEventHandler(eventEmitter,notifyService);
+let hostEventHandler = new HostEventHandler(eventEmitter, notifyService);
 let timer = new Timer(eventEmitter);
 timer.start(3000);
 
-let monitor = new Monitor(eventEmitter);
+const monitorService = require('./services/monitorService');
+monitorService.use('nmap', require('./services/nmapService'));
+monitorService.use('ping', require('./services/nmapService'));
+
+let monitor = new Monitor(eventEmitter, monitorService);
 eventEmitter.on('timeout', _ => {
     monitor.inspectHosts();
 });
