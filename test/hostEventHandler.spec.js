96d5c4fcd3d1e7f6531c4bb9019e9da1fdf31c8c
MOONYAN <1787hz@gmail.com>
2018-05-13 11:51:52 +0800

    add hostEventHandler test case

---
+++ test/hostEventHandler.spec.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const should = chai.should();
+const HostEventHadler = require('../domain/hostEventHandler');
+const EventEmitter = require('events');
+
+describe('HostEventHadler', _ => {
+    it('should handle host event', done => {
+        let emitter = new EventEmitter();
+        let notifyService = {
+            notify(contact, subject) {
+                contact.should.have.property('address');
+                subject.should.have.property('name');
+                subject.should.have.property('status');
+                done();
+            }
+        };
+        let handler = new HostEventHadler(emitter, notifyService);
+        let host = {
+            name:'Latte',
+            status:'UP',
+            contacts: [{
+                address: {
+                    Email: 'trump@gmail.com',
+                    Phone: '0800-092-000'
+                }
+            }] 
+        }
+        emitter.emit('statusChange',host);
+    });
+});
