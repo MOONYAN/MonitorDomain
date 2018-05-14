3e1c9b5e8a00f5d7186fbca102c9043c45ac654c
MOONYAN <1787hz@gmail.com>
2018-05-14 14:57:44 +0800

    add hostService test case 'updateHostCommand'

--- test/hostService.spec.js
+++ test/hostService.spec.js
@@ -32,4 +32,34 @@ describe('HostService', _ => {
             done(err);
         });
     })
+
+    it('should update host command', done => {
+        let hostService = new HostService({
+            async updateHostCommand({id,command}) {
+                return {
+                    id: id,
+                    name: 'Trump',
+                    command: command,
+                    ip: '',
+                    status: 'Unknown',
+                    contact: []
+                }
+            }
+        });
+
+        hostService.updateHostCommand({
+            id:'9487',
+            command: 'Yee'
+        }).then((newHost) => {
+            newHost.should.have.property('id');
+            newHost.should.have.property('name');
+            newHost.should.have.property('command').with.equal('Yee');
+            newHost.should.have.property('ip');
+            newHost.should.have.property('status');
+            newHost.should.have.property('contact').with.lengthOf(0);
+            done();
+        }, err => {
+            done(err);
+        });
+    })
 });
