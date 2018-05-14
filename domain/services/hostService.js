7966da347cdb479996bcc461c99e9f4307750064
MOONYAN <1787hz@gmail.com>
2018-05-14 11:47:50 +0800

    DI host service

--- domain/services/hostService.js
+++ domain/services/hostService.js
@@ -1,83 +1,53 @@
-var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://Sasuke:9487@ds033126.mlab.com:33126/narutomongo');
+module.exports = class HostService {
+    constructor(proxy) {
+        this.service = proxy;
+    };
 
-var HostSchema = require('../Schemas/hostSchema');
-var ContactSchema = require('../Schemas/contactSchema');
-var Host = require('../host');
+    async addHost(host) {
+        try {
+            return await this.service.addHost(host);
+        } catch (err) {
+            throw err;
+        }
+    };
 
-var self = {};
+    async updateHostCommand(host) {
+        try {
+            return await this.service.updateHostCommand(host);
+        } catch (err) {
+            throw err;
+        }
+    };
 
-self.addHost = async (host) => {
-    try {
-        return new Host(await HostSchema.create({
-            name: host.name,
-            command: host.command,
-            ip: host.ip,
-            status: host.status
-        }));
-    } catch (err) {
-        throw err;
+    async updateHostStatus(host) {
+        try {
+            return await this.service.updateHostStatus(host);
+        } catch (err) {
+            throw err;
+        }
+    };
+
+    async attachContact(host, contact) {
+        try {
+            return await this.service.attachContact(host, contact);
+        } catch (err) {
+            throw err;
+        }
     }
-};
 
-self.updateHostCommand = async (host) => {
-    try {
-        return new Host(await HostSchema.findByIdAndUpdate(host.id, {
-            '$set': {
-                'command': host.command
-            }
-        }, {
-            new: true
-        }).populate('contacts').exec());
-    } catch (err) {
-        throw err;
-    }
-};
+    async getHosts() {
+        try {
+            return await this.service.getHosts();
+        } catch (err) {
+            throw err;
+        }
+    };
 
-self.updateHostStatus = async (host) => {
-    try {
-        return new Host(await HostSchema.findByIdAndUpdate(host.id, {
-            '$set': {
-                'ip': host.ip,
-                'status': host.status
-            }
-        }, {
-            new: true
-        }).populate('contacts').exec());
-    } catch (err) {
-        throw err;
-    }
+    async findHost(key) {
+        try {
+            return await this.service.findHost();
+        } catch (err) {
+            throw err;
+        }
+    };
 }
-
-self.attachContact = async (host, contact) => {
-    try {
-        return new Host(await HostSchema.findByIdAndUpdate(host.id, {
-            '$addToSet': {
-                "contacts": contact.id
-            }
-        }, {
-            new: true
-        }).populate('contacts').exec());
-    } catch (err) {
-        throw err;
-    }
-}
-
-self.getHosts = async () => {
-    try {
-        let hosts = await HostSchema.find().populate('contacts').exec();
-        return hosts.map(host => new Host(host));
-    } catch (err) {
-        throw err;
-    }
-};
-
-self.findHost = async (key) => {
-    try {
-        return new Host(await HostSchema.findById(key).populate('contacts').exec());
-    } catch (err) {
-        throw err;
-    }
-};
-
-module.exports = self;
