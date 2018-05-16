db47de30f5fbf6a9c9b84cdec33e5f157e6dbb84
MOONYAN <1787hz@gmail.com>
2018-05-16 19:20:51 +0800

    JSDOC iSender

--- domain/services/phoneService.js
+++ domain/services/phoneService.js
@@ -1,4 +1,9 @@
 module.exports = {
+    /**
+     * 
+     * @param {string} Phone 
+     * @param {*} message 
+     */
     send(Phone, message) {
         console.log(`Send Phone ${Phone} ->`, message);
     }
