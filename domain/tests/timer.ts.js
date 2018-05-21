78c82cf6b73f7b3b1f7c97d2dbd26b1d997b8628
MOONYAN <1787hz@gmail.com>
2018-05-21 16:50:08 +0800

    remove unused file

--- domain/tests/timer.ts.js
+++
@@ -1,8 +0,0 @@
-var timer = require('../timer');
-var messageBus = require('../messageBus');
-
-messageBus.subscribe((event) => {
-    if (event.eventType == 'onTimeoutEvent') {
-        console.log(event);
-    }
-});
